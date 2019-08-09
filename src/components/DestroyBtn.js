754845d4c40ffe3f534cc78a8f92b673e9680b3c
Dylan Moylan <dylanmoylan1@gmail.com>
2019-08-09 09:36:52 -0400

    Adds functioning destroy button.

--- src/components/DestroyBtn.js
+++ src/components/DestroyBtn.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 function DestroyBtn(props) {
   return (
-    <div className="destroy-button">
+    <div className="destroy-button" onClick={(e) => {props.removeTodo(e,props.index)}}>
+      <span>X</span>
     </div>
   )
 }
--- src/containers/TodoList.js
+++ src/containers/TodoList.js
@@ -10,11 +10,15 @@ class TodoList extends React.Component {
       todos: []
     }
     this.addTodo = this.addTodo.bind(this)
-    this.removeTodo = this.removeTodo(this)
+    this.removeTodo = this.removeTodo.bind(this)
+    this.setComplete = this.setComplete.bind(this)
   }
 
   addTodo(value) {
-    let newTodo = {text:value,active:true}
+    let newTodo = {
+      text:value,
+      completed: false
+    }
     let newTodos = this.state.todos;
     newTodos.push(newTodo)
     this.setState({
@@ -22,18 +26,33 @@ class TodoList extends React.Component {
     })
   }
 
-  removeTodo(index) {
+  setComplete(index) {
+    let todos = [...this.state.todos]
+    todos[index].completed = true;
+    this.setState({
+      todos: todos
+    })
+  }
 
+  removeTodo(e,index) {
+    let todos = [...this.state.todos]
+    let newTodos = todos.splice(index,1)
+    this.setState({
+      todos: todos
+    })
   }
 
   render() {
     return (
       <div className="todos">
         <NewTodo addTodo={this.addTodo} />
-        <div className={ this.state.todos.length ? 'todo-list' : 'todo-list nodisplay'}>
+        <div className={ this.state.todos.length > 0 ? 'todo-list' : 'todo-list nodisplay'}>
           <ul>
             {this.state.todos.map((item, index) => {
-              return <li key={index}><p>{item.text}</p><DestroyBtn removeTodo={this.removeTodo} index={index} /></li>
+              return <li key={index}>
+                        <div onClick={(e) => {this.setComplete(index)}}>{item.text}</div>
+                        <DestroyBtn removeTodo={this.removeTodo} index={index} />
+                      </li>
             })}
           </ul>
         </div>
